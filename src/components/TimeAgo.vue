06a29be4481ced56146f7f1c62fb6e35510b03ba
Edward Hails <onfe@users.noreply.github.com>
2019-06-23 12:39:24 +0100

    Add TimeAgo component.

---
+++ src/components/TimeAgo.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>Last online {{ timeText }}</span>
+</template>
+
+<script>
+import ta from "time-ago";
+
+export default {
+  name: "TimeAgo",
+  props: {
+    time: Date
+  },
+  computed: {
+    timeText: function() {
+      console.log(this.time);
+      return ta.ago(this.time);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.state-icon {
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: bottom;
+}
+
+.ok {
+  color: var(--success);
+}
+
+.unknown {
+  color: var(--secondary);
+}
+
+.ko {
+  color: var(--danger);
+}
+</style>
