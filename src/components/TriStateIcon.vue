baa7154a0914ea6d89032e52a517f2ea0de6217d
Edward Hails <onfe@users.noreply.github.com>
2019-06-23 12:39:37 +0100

    Add TriStateIcon component.

---
+++ src/components/TriStateIcon.vue
@@ -0,0 +1,48 @@
+<template>
+  <a class="btn-icn">
+    <font-awesome-icon v-if="isOK" class="state-icon ok" icon="check-circle" />
+    <font-awesome-icon v-if="isKO" class="state-icon ko" icon="times-circle" />
+    <font-awesome-icon
+      v-if="!isKO && !isOK"
+      class="state-icon unknown"
+      :icon="['far', 'question-circle']"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  name: "TriStateIcon",
+  props: {
+    status: String
+  },
+  computed: {
+    isOK: function() {
+      return this.status == "ok";
+    },
+    isKO: function() {
+      return this.status == "ko";
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.state-icon {
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: bottom;
+}
+
+.ok {
+  color: var(--success);
+}
+
+.unknown {
+  color: var(--secondary);
+}
+
+.ko {
+  color: var(--danger);
+}
+</style>
