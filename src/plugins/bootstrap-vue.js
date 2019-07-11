f3610dd10dac049bee4a227216fd2106e350b1e1
Edward Hails <onfe@users.noreply.github.com>
2019-07-11 16:33:22 +0100

    Reduce production bundle size by 25% (836KiB to 633KiB)

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,24 @@
 import Vue from "vue";
 
-import BootstrapVue from "bootstrap-vue";
+import {
+  LayoutPlugin,
+  CardPlugin,
+  NavbarPlugin,
+  PopoverPlugin,
+  ButtonPlugin,
+  FormInputPlugin,
+  FormPlugin,
+  FormGroupPlugin
+} from "bootstrap-vue";
+
+Vue.use(LayoutPlugin);
+Vue.use(CardPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(PopoverPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormPlugin);
+Vue.use(FormGroupPlugin);
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
-Vue.use(BootstrapVue);
