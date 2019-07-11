eaa27aa38e2115793aa4749890556036b0196b2e
Edward Hails <onfe@users.noreply.github.com>
2019-07-11 23:39:12 +0100

    Further reduce production bundle size by 10% (633KiB to 573KiB)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const configureServer = require("./src/server/config/configure");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin;
 
 module.exports = {
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()]
+  },
   devServer: {
     before: configureServer
   }
