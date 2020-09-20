6e58520385c17e1928a7945cecc683e0ada51bee
Edward Hails <onfe@users.noreply.github.com>
2020-09-20 20:23:44 +0100

    Add basic mutation resolvers.

--- server/resolvers/mutation.js
+++ server/resolvers/mutation.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const bcrypt = require("bcryptjs");
+const { context } = require("../api");
 const tokens = require("../tokens");
 
 const Mutation = {
@@ -18,7 +19,6 @@ const Mutation = {
   },
 
   login: async (_, {username, password}) => {
-    console.log(username);
     const user = await prisma.user.findOne({
       where: {
         username
@@ -27,10 +27,111 @@ const Mutation = {
 
     // if the user exists and the password matches, create an auth token.
     if (user && await bcrypt.compare(password, user.passHash)) {
-      return tokens.create(user, 3600);
+      return tokens.create(user);
     } else {
       return null;
     }
+  },
+
+  createGroup: async (_, { name }, ctx) => {
+    if (!ctx.user) return null;
+    if (name.length === 0) return null;
+    const group = await prisma.group.create({
+      data: {
+        name,
+        members: {
+          connect: { id: ctx.user.id }
+        }
+      }
+    })
+    return group;
+  },
+  leaveGroup: async (_, { id }, ctx) => {
+    if (!ctx.user) return null;
+    const group = await prisma.group.update({
+      where: { id },
+      data: {
+        members: {
+          disconnect: [ { id: ctx.user.id }]
+        } 
+      },
+      include: {
+        members: {
+          select: {
+            id: true
+          }
+        }
+      }
+    }).catch( () => {
+      return null;
+    });
+
+    // if the group is now empty, delete it.
+    if (group && group.members.length === 0) {
+      await prisma.group.delete({ where: { id }});
+    }
+    return group;
+  },
+
+  deleteGroup: async (_, { id }) => {
+    return await prisma.group.delete({ where: { id }});
+  },
+
+  addUserToGroup: async (_, { userId, groupId }) => {
+    return await prisma.group.update({
+      where: { id: groupId },
+      data: {
+        members: {
+          connect: { id: userId }
+        }
+      }
+    })
+  },
+
+  addDeviceToGroup: async (_, { deviceId, groupId }) => {
+    return await prisma.group.update({
+      where: { id: groupId },
+      data: {
+        devices: {
+          connect: { id: deviceId }
+        }
+      }
+    })
+  },
+
+  removeDeviceFromGroup: async (_, { deviceId, groupId }) => {
+    return await prisma.group.update({
+      where: { id: groupId },
+      data: {
+        devices: {
+          disconnect: { id: deviceId }
+        }
+      }
+    })
+  },
+
+  addDevice: async (_, { name }, ctx) => {
+    if (!ctx.user) return null;
+    return prisma.device.create({
+      data: {
+        name,
+        owner: {
+          connect: { id: ctx.user.id }
+        }
+      }
+    })
+  },
+
+  removeDevice: async (_, { id }) => {
+    const result = await prisma.device.delete({ where: { id } })
+    return !!result;
+  },
+
+  renameDevice: async (_, { id, nickname }) => {
+    return await prisma.device.update({
+      where: { id },
+      data: { nickname }
+    })
   }
 }
 
